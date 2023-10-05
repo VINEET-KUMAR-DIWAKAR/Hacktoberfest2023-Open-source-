0b37979738e0f50a1e01a3cd3e1d37142a04492f
VINEET-KUMAR-DIWAKAR <114769998+VINEET-KUMAR-DIWAKAR@users.noreply.github.com>
2023-10-05 16:19:28 +0530

    Create powerOfNumber.c
    Power of a Number in C.

---
+++ powerOfNumber.c
@@ -0,0 +1,28 @@
+// Power of a Number in C
+// calculate pow(x, n) 
+long power(int x, unsigned n) 
+{ 
+    // Initialize result to 1 
+    long long pow = 1; 
+  
+    // Multiply x for n times 
+    for (int i = 0; i < n; i++) { 
+        pow = pow * x; 
+    } 
+  
+    return pow; 
+} 
+  
+// Driver code 
+int main(void) 
+{ 
+  
+    int x = 2; 
+    unsigned n = 3; 
+  
+    // Function call 
+    int result = power(x, n); 
+    printf("%d", result); 
+  
+    return 0; 
+}
